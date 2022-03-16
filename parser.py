056a38a0ee6a121c6455fd5cc90caf2e50288493
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2022-03-16 10:28:59 -0300

    filtrando Zr

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import os
+import json
 import numpy as np
+from tqdm import tqdm 
 import pandas as pd
 import matplotlib.pyplot as plt
 from pymatgen.core import Structure
@@ -10,18 +12,44 @@ calculator = XRDCalculator(
     wavelength="CuKa"
 )
 
-os.chdir("data")
-maxes=set()
-patterns = {}
-for file in os.listdir():
-    try: 
-        structure = CifParser(file).get_structures()[0]
-        xrd = calculator.get_pattern(structure)
-    except Exception as e :
-        print(e)
-    maxes.add(xrd.y.max())
-    patterns[file]= xrd.x,xrd.y
-print(maxes)
+def load_data(filename):
+    os.chdir('/home/gonik/Documents/git/xrd_match')
+    return pd.read_csv(filename)
+    
+
+
+def filter_Zr():
+    """
+    Filter out structures with Zr
+    """
+    
+    # os.chdir("")
+    structure = load_data("2019-07-01-FSR-public_7061.csv")
+    return [f"{row.filename}.cif" for n,row in df.iterrows() if "Zr" in row["All_Metals"]]
 
 
 
+
+def main(filter = True):
+    os.chdir("data")
+    maxes=set()
+    patterns = {}
+    if filter:
+        filenames = filter_Zr()
+    else:
+        filenames = os.listdir()
+    for file in tqdm(filenames):
+        try: 
+            structure = CifParser(file).get_structures()[0]
+            xrd = calculator.get_pattern(structure)
+        except Exception as e :
+            print(e)
+        maxes.add(xrd.y.max())
+        patterns[file]= xrd.x,xrd.y
+    print(maxes)
+    with open("diffraction_patterns.json","w") as file:
+        json.dump(patterns,file)
+
+
+if __name__ == "__main__":
+    main()
