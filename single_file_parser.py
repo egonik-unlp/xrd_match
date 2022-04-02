6a82488b3eb85726eacbdae05cc10025d75f3020
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2022-04-02 19:51:09 -0300

    agrego para que funcione con bash

---
+++ single_file_parser.py
@@ -0,0 +1,41 @@
+#!/home/gonik/anaconda3/envs/xrd/bin/python
+
+from pymatgen.io.cif import CifParser
+from pymatgen.core.structure import Structure
+from pymatgen.analysis.diffraction.xrd import XRDCalculator
+import sys
+import json
+
+
+def main(file):
+    try:
+        print("file = {}".format(file))
+        structure = CifParser(file).get_structures()[0]
+        xrd = XRDCalculator().get_pattern(structure)
+        pat = [xrd.x.tolist(), xrd.y.tolist()]
+        file = file.split("/")[-1]
+        fn = f"data/ip/{file[:-4]}.json"
+        with open(fn, "w") as f:
+            json.dump(pat,f)
+        print(f"{file} -> {fn}")
+
+    except Exception as e:
+        print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+
+
